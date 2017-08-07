bd075ad365e291442bfa1ddefd3165990dd81a08
Dallan Quass <dallan@quass.org>
2017-08-07 12:10:47 -0600

    force https

--- html/index.php
+++ html/index.php
@@ -1,8 +1,16 @@
+<?php
+if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] != "https"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+?>
 <html>
 <title>Family Tree Explorer</title>
 <frameset cols="370,*">
-<frame src="/fte/FTE.php?userName=<?php echo urlencode(@$_REQUEST['userName']); ?>&treeName=<?php echo urlencode(@$_REQUEST['treeName']); ?>&page=<?php echo urlencode(@$_REQUEST['page']); ?>" name="fte"/>
-<frame src="/wiki/WeRelate:Family_Tree_Explorer" name="content"/>
+<frame src="https://www.werelate.org/fte/FTE.php?userName=<?php echo urlencode(@$_REQUEST['userName']); ?>&treeName=<?php echo urlencode(@$_REQUEST['treeName']); ?>&page=<?php echo urlencode(@$_REQUEST['page']); ?>" name="fte"/>
+<frame src="https://www.werelate.org/wiki/WeRelate:Family_Tree_Explorer" name="content"/>
 <noframes>
 <body>Your browser must support frames in order to use Family Tree Explorer</body>
 </noframes>
